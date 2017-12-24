90a3ef22c1c871239941058bf2375773c8b63817
Angel Contreras <angelcbenoit@gmail.com>
2017-12-24 13:25:32 -0800

    updated javascript

--- calc.js
+++ calc.js
@@ -2,17 +2,32 @@ $(document).ready(function () {
 
     var firstNumber = [];
     var secondNumber = [];
+    var expression = "";
 
     //checks for decimal
     var decimal = false;
 
     $("td").on("click", function(){
         var item = $(this).text();
-        item = Number(item);
-        if(Number.isInteger(item)){
-            firstNumber.push(item);
+        var num = Number(item);
+        if(Number.isInteger(num)){
+            firstNumber.push(num);
             console.log("TEST");
             update(firstNumber);
+        }
+        else if(item === "+" || item === "-" || item === "*" || item === "/"){
+            expression = item;
+            secondNumber = firstNumber;
+            firstNumber = [];
+            $("#textSmall").text(secondNumber + expression);
+            //updateSmallText(secondNumber);
+        }
+        else if(item === "=" && expression.length === 1){
+            var num1 = Number(secondNumber);
+            var num2 = Number(firstNumber);
+            var result = eval(num1.toString() + expression + num2.toString());
+            console.log(result);
+            $("#text").text(result.toFixed(2));
         }
     });
 
@@ -24,10 +39,7 @@ $(document).ready(function () {
     });
 
     $("#clear").on("click", function () {
-        decimal = false;
-        firstNumber = [];
-        secondNumber = [];
-        $("#text").text("0");
+        clear();
     });
 
 
@@ -38,9 +50,18 @@ $(document).ready(function () {
     }
 
     function updateSmallText(num) {
-        $("#textSmall").text(num);
+        var display = num.join("");
+        $("#textSmall").text(display);
     }
 
+    function clear() {
+        decimal = false;
+        firstNumber = [];
+        secondNumber = [];
+        expression = "";
+        $("#text").text("0");
+        $("#textSmall").text("0");
+    }
 
 });
 
