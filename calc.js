54e6bb388baf67f64e96a940f9a598f307900dcb
Angel Contreras <angelcbenoit@gmail.com>
2017-12-24 16:42:49 -0800

    updated javascript and user interface

--- calc.js
+++ calc.js
@@ -1,39 +1,54 @@
 $(document).ready(function () {
 
     var firstNumber = [];
-    var secondNumber = [];
+    var secondNumber = 0;
     var expression = "";
 
     //checks for decimal
     var decimal = false;
+    var negative = false;
 
     $("td").on("click", function(){
         var item = $(this).text();
         var num = Number(item);
         if(Number.isInteger(num)){
             firstNumber.push(num);
-            console.log("TEST");
             update(firstNumber);
+        }
+        else if(item === "±"){
+            if(negative === false){
+                firstNumber.unshift("-");
+                update(firstNumber);
+                negative = true;
+            }
         }
         else if(item === "+" || item === "-" || item === "*" || item === "/"){
             expression = item;
-            secondNumber = firstNumber;
+            secondNumber = firstNumber.join("");
             firstNumber = [];
-            $("#textSmall").text(secondNumber + expression);
-            //updateSmallText(secondNumber);
+            decimal = false;
+            negative = false;
+            updateSmallText(secondNumber, expression);
         }
         else if(item === "=" && expression.length === 1){
             var num1 = Number(secondNumber);
-            var num2 = Number(firstNumber);
+            var num2 = Number(firstNumber.join(""));
             var result = eval(num1.toString() + expression + num2.toString());
-            console.log(result);
-            $("#text").text(result.toFixed(2));
+            if(expression === "/" && num2 === 0){
+                $("#text").text("Cannot divide by 0");
+            }
+            else{
+                $("#text").text(result.toFixed(2));
+            }
+
         }
     });
+
 
     $("#decimal").on("click", function () {
         if(decimal === false){
             firstNumber.push(".");
+            update(firstNumber);
             decimal = true;
         }
     });
@@ -49,13 +64,13 @@ $(document).ready(function () {
         $("#text").text(display);
     }
 
-    function updateSmallText(num) {
-        var display = num.join("");
-        $("#textSmall").text(display);
+    function updateSmallText(num, exp) {
+        $("#textSmall").text(num + exp);
     }
 
     function clear() {
         decimal = false;
+        negative = false;
         firstNumber = [];
         secondNumber = [];
         expression = "";
